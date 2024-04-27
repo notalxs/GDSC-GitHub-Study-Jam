edf250d84e5687fdd06a607281b91889097d3e09
notalxs <fionnacoralde@gmail.com>
2024-04-27 18:02:44 +0800

    added details and message

---
+++ Study_Jam.java
@@ -0,0 +1,14 @@
+public class Study_Jam {
+    public static void main(String[] args) {
+        System.out.println("\n\tWELCOME TO GDSC STUDY JAM <3");
+
+        System.out.println("\nNAME: Fionna Alexis P. Coralde");
+        System.out.println("COURSE: BS in Information Technology");
+        System.out.println("YEAR: Second");
+        System.out.println("SECTION: 2-2N");
+
+        String message = "\n\tLet's Go Software Development Team!";
+        System.out.println(message);
+    }
+}
+
